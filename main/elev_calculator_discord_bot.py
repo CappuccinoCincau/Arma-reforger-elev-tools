96fea39af2076ed3b06f8a0dd91cc5efd21f2ef2
CappuccinoCincau <devintjendra@gmail.com>
2026-03-23 09:22:35 +0700

    Remove some unnecessary functions

--- main/elev_calculator_discord_bot.py
+++ main/elev_calculator_discord_bot.py
@@ -3,32 +3,6 @@ import nacl
 
 PUBLIC_KEY          = os.environ["DISCORD_PUBLIC_KEY"]
 DISCORD_API_BASE    = "https://discord.com/api/v10"
-CLIENT_ID = ''
-CLIENT_SECRET = ''
-
-def get_token():
-    data = {
-        'grant_type': 'client_credentials',
-        'scope': 'identify connections'
-    }
-    headers = {
-        'Content-Type': 'application/x-www-form-urlencoded'
-    }
-    r = requests.post('%s/oauth2/token' % DISCORD_API_BASE, data=data, headers=headers, auth=(CLIENT_ID, CLIENT_SECRET))
-    r.raise_for_status()
-    return r.json()
-
-def refresh_token(refresh_token):
-    data = {
-    'grant_type': 'refresh_token',
-    'refresh_token': refresh_token
-        }
-    headers = {
-        'Content-Type': 'application/x-www-form-urlencoded'
-        }
-    r = requests.post('%s/oauth2/token' % DISCORD_API_BASE, data=data, headers=headers, auth=(CLIENT_ID, CLIENT_SECRET))
-    r.raise_for_status()
-    return r.json()
 
 def verify_key(body: bytes, sig: str, timestamp: str, public_key: str) -> bool:
     """Return True if the Ed25519 signature is valid for the given body."""
